bc202e836d4e78248f7534043c8a5c5ee96d6c02
Theo LEBRUN <mib.ftw@gmail.com>
2024-08-19 01:19:51 -0400

    Use default VPC and create required components

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import os
+
 import aws_cdk as cdk
+from dotenv import load_dotenv
 
 from delta_upgrade_bot.delta_upgrade_bot_stack import DeltaUpgradeBotStack
 
+load_dotenv()
+
 app = cdk.App()
-env = cdk.Environment(account="", region="us-east-1")
+env = cdk.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"], region=os.environ["CDK_DEFAULT_REGION"])
 DeltaUpgradeBotStack(app, "DeltaUpgradeBotStack", env=env)
 
 app.synth()
--- delta_upgrade_bot/delta_upgrade_bot_stack.py
+++ delta_upgrade_bot/delta_upgrade_bot_stack.py
@@ -1,3 +1,5 @@
+import os
+
 from aws_cdk import (
     BundlingOptions,
     Duration,
@@ -6,7 +8,9 @@ from aws_cdk import (
     aws_events_targets as targets,
     aws_events as events,
     aws_sns as sns,
+    aws_sns_subscriptions as sns_subscriptions,
     aws_s3 as s3,
+    aws_s3_deployment as s3_deployment,
     aws_ec2 as ec2,
 )
 from constructs import Construct
@@ -17,26 +21,27 @@ class DeltaUpgradeBotStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)
-        subnet = ec2.Subnet.from_subnet_attributes(
-            self, "Subnet", subnet_id="", availability_zone="us-east-1a"
-        )
-        sg = ec2.SecurityGroup.from_security_group_id(
-            self, "SG", ""
-        )
+        name = "DeltaUpgradeBot"
 
-        bucket = s3.Bucket.from_bucket_arn(
-            self, "Bucket", ""
-        )
-        topic = sns.Topic.from_topic_arn(
-            self, "Topic", ""
-        )
+        # Network
+        vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)
+        subnet = vpc.select_subnets(subnet_type=ec2.SubnetType.PUBLIC).subnets[0]
+        sg = ec2.SecurityGroup.from_lookup_by_name(self, "SecurityGroup", security_group_name="default", vpc=vpc)
+
+        # Bucket
+        bucket = s3.Bucket(self, name)
+        s3_deployment.BucketDeployment(self, "DeployConfig", destination_bucket=bucket,
+                                       sources=[s3_deployment.Source.asset('delta_upgrade_bot/bucket')])
+
+        # SNS
+        topic = sns.Topic(self, f"{name}Email")
+        topic.add_subscription(sns_subscriptions.EmailSubscription(os.environ["CDK_SNS_EMAIL"]))
 
         lambda_fn = lambda_.Function(
             self,
-            "Lambda",
+            f"{name}Lambda",
             memory_size=512,
-            function_name="DeltaUpgradeBot",
+            function_name=name,
             code=lambda_.Code.from_asset(
                 "./lambda",
                 bundling=BundlingOptions(
@@ -66,7 +71,7 @@ class DeltaUpgradeBotStack(Stack):
 
         rule = events.Rule(
             self,
-            "Rule",
+            f"{name}Rule",
             schedule=events.Schedule.rate(Duration.minutes(30)),
         )
         rule.add_target(targets.LambdaFunction(lambda_fn))
